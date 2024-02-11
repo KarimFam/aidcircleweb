7ef66bbe84688632d2999b36bcfa953afd2fc300
Shahan <44978325+blueflightx7@users.noreply.github.com>
2024-02-11 16:25:26 -0600

    initial model creation for entities in the domain project

---
+++ H4H.Domain/Entities/Address.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+using H4H.Domain.Enums;
+
+namespace H4H.Domain.Entities
+{
+    public class Address : BaseEntity
+    {
+        [JsonPropertyName("line1")]
+        public string Line1 { get; set; }
+
+        [JsonPropertyName("line2")]
+        public string Line2 { get; set; }
+
+        [JsonPropertyName("city")]
+        public string City { get; set; }
+
+        [JsonPropertyName("state")]
+        public string State { get; set; }
+
+        [JsonPropertyName("postalCode")]
+        public string PostalCode { get; set; }
+
+        [JsonPropertyName("country")]
+        public string Country { get; set; }
+
+        [JsonPropertyName("addressType")]
+        public AddressType AddressType { get; set; } // Assuming AddressType is an enum
+
+        // Relationship to Organization
+        [JsonPropertyName("organizationId")]
+        public int OrganizationId { get; set; }
+        [ForeignKey("OrganizationId")]
+        public virtual Organization Organization { get; set; }
+    }
+}
+
---
+++ H4H.Domain/Entities/User.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace H4H.Domain.Entities
+{
+    public class User : BaseEntity
+    {
+        [JsonPropertyName("username")]
+        public string Username { get; set; }
+
+        [JsonPropertyName("email")]
+        public string Email { get; set; }
+
+        [JsonPropertyName("firstName")]
+        public string FirstName { get; set; }
+
+        [JsonPropertyName("lastName")]
+        public string LastName { get; set; }
+
+        [JsonPropertyName("dateOfBirth")]
+        public DateTime DateOfBirth { get; set; }
+
+        [JsonPropertyName("isActive")]
+        public bool IsActive { get; set; }
+
+        // External Authentication
+        [JsonPropertyName("externalAuthProvider")]
+        public string ExternalAuthProvider { get; set; }
+
+        [JsonPropertyName("externalAuthId")]
+        public string ExternalAuthId { get; set; }
+
+        // User-specific properties (if any)
+    }
+
+
+}
