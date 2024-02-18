8e0f0e90af163926abb8778615a029e84be914e3
Shahan <44978325+blueflightx7@users.noreply.github.com>
2024-02-18 17:48:16 -0600

    Added organization interface and repository

---
+++ H4H.Infrastructure/Repositories/OrganizationRepository.cs
@@ -0,0 +1,52 @@
+﻿using H4H.Domain.Entities;
+using H4H.Domain.Interfaces;
+using H4H.Infrastructure.Data;
+using H4H.Infrastructure.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace H4H.Infrastructure.Repositories
+{
+    public class OrganizationRepository : IOrganizationRepository
+    {
+        private readonly H4HDbContext _context;
+
+        public OrganizationRepository(H4HDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Organization> GetByIdAsync(int id)
+        {
+            return await _context.Organizations
+                                 .Include(org => org.Addresses) // Include related entities as needed
+                                 .FirstOrDefaultAsync(org => org.Id == id);
+        }
+
+        public async Task<List<Organization>> GetAllAsync()
+        {
+            return await _context.Organizations
+                                 .Include(org => org.Addresses) // Include related entities as needed
+                                 .ToListAsync();
+        }
+
+        public async Task AddAsync(Organization organization)
+        {
+            await _context.Organizations.AddAsync(organization);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Organization organization)
+        {
+            _context.Organizations.Update(organization);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Organization organization)
+        {
+            _context.Organizations.Remove(organization);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
