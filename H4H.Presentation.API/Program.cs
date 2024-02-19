b4871642d239ae4e34e979298995b21c80f54b64
Shahan <44978325+blueflightx7@users.noreply.github.com>
2024-02-18 18:52:34 -0600

    Initial depdendency injections added

--- H4H.Presentation.API/Program.cs
+++ H4H.Presentation.API/Program.cs
@@ -1,6 +1,31 @@
+using H4H.Application.Interfaces;
+using H4H.Application.Services;
+using H4H.Domain.Interfaces;
+using H4H.Infrastructure.Data.Contexts;
+using H4H.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<H4HDbContext>(options =>
+       options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+// Repositories
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IVolunteerRepository, VolunteerRepository>();
+builder.Services.AddScoped<IOrganizationRepository, OrganizationRepository>();
+builder.Services.AddScoped<IItemRepository, ItemRepository>();
+builder.Services.AddScoped<IAddressRepository, AddressRepository>();
+
+// Services
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IVolunteerService, VolunteerService>();
+builder.Services.AddScoped<IOrganizationService, OrganizationService>();
+builder.Services.AddScoped<IItemService, ItemService>();
+builder.Services.AddScoped<IAddressService, AddressService>();
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
