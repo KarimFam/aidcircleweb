8b45f26d9425445105aef1cf54b30958afa4904a
Humza K <hk@karimfam.com>
2024-08-03 19:46:31 -0400

    created itemDto. Need to edit still.

--- H4H.Presentation.API/Data/AddressDto.cs
+++ H4H.Presentation.API/Data/AddressDto.cs
@@ -5,11 +5,11 @@ namespace H4H.Presentation.API.Data
 {
     public class AddressDto
     {
-        public Guid AddressId { get; set; }
+      //  public Guid AddressId { get; set; }
        // public string Street { get; set; }
         public string City { get; set; }
         public string State { get; set; }
-        public string ZipCode { get; set; }
+      //  public string ZipCode { get; set; }
 
         public string Country {  get; set; }
         public string PostalCode   { get; set; }
---
+++ H4H.Presentation.API/Data/ItemDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace H4H.Presentation.API.Data
+{
+    public class ItemDto
+    {
+      //  public Guid ItemId { get; set; }
+        public String Name { get; set; }
+        public String Description { get; set; }
+        public Guid UserId { get; set; }
+        public Guid? OrganizationId { get; set; }
+   //     public Guid? ItemId { get; set; }
+        public Guid? OrderId { get; set; }
+    
+        public AddressDto Address { get; set; }
+
+
+    }
+}
